8e539427d0091488933a6b7c0141bd8daf21a006
Ken OKABE <kenokabe@gmail.com>
2014-01-16 18:06:42 +0900

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,21 +1,24 @@
 var WebSocketServer = require('ws')
-	.Server;
+    .Server;
 
 var port = 7000;
 var wss = new WebSocketServer(
 {
-	port: port
+    port: port
 });
 
 console.log('ws echo server: ' + port);
 
-wss.on('connection', function(ws)
-{
-	ws.on('message', function(message)
-	{
-		console.log('received: %s', message);
+wss
+    .on('connection', function(ws)
+    {
+        console.log('-----open!!-----');
+        ws
+            .on('message', function(message)
+            {
+                console.log('received: %s', message);
 
 
-		ws.send(message);
-	});
-});
+                ws.send(message);
+            });
+    });
